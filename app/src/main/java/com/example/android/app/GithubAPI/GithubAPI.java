4c9fdf27b879ab2d07d03a48bb15ea761170ddb4
vitols <vitoleg1@yandex.ru>
2018-02-01 12:41:46 +0300

    Refactor code

--- app/src/main/java/com/example/android/app/GithubAPI/GithubAPI.java
+++ app/src/main/java/com/example/android/app/GithubAPI/GithubAPI.java
@@ -1,5 +1,7 @@
 package com.example.android.app.GithubAPI;
 
+import android.util.Log;
+
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 import java.io.IOException;
@@ -18,9 +20,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class GithubAPI {
-
-    private static GithubAPIInterface gitApi;
-    private Retrofit retrofit;
 
     private static final String CLIENT_ID = "549a86fc3d662f6da6b8";
     private static final String CLIENT_SECRET = "df93a026c9c1d0bad473a2b84920f0bd47f9186e";
@@ -49,6 +48,12 @@ public class GithubAPI {
             }
         });
 
+        HttpLoggingInterceptor interceptor2 = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        okHttpClient.addInterceptor(interceptor);
+
+        Log.v("request: ", okHttpClient.toString());
+
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(url)
@@ -56,6 +61,7 @@ public class GithubAPI {
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .client(okHttpClient.build())
                 .build();
+
 
         return retrofit.create(GithubAPIInterface.class);
     }
